fd4bdc736da9f2fefc0c836360ad33d90665e6e3
M4uri-000 <mauricioetp6@gmail.com>
2024-12-10 15:00:36 -0300

    Cambiando la interfaz del menu principal

--- app/src/main/java/cl/vmi/adi/MainActivity.java
+++ app/src/main/java/cl/vmi/adi/MainActivity.java
@@ -8,54 +8,111 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView welcomeMessage;
-    private Button buttonCategoria, buttonProveedor, buttonProducto, buttonLogout;
     private FirebaseAuth auth;
+    private Button buttonCategoria, buttonProveedor, buttonProducto, buttonReporte, buttonInventario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Configura el Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Inicialización de FirebaseAuth
         auth = FirebaseAuth.getInstance();
-        welcomeMessage = findViewById(R.id.welcome_message);
+
+        // Enlazando los botones
         buttonCategoria = findViewById(R.id.buttonCategoria);
         buttonProveedor = findViewById(R.id.buttonProveedor);
         buttonProducto = findViewById(R.id.buttonProducto);
-        buttonLogout = findViewById(R.id.buttonLogout);
+        buttonReporte = findViewById(R.id.buttonReporte);
+        buttonInventario = findViewById(R.id.buttonInventario);
 
-        FirebaseUser user = auth.getCurrentUser();
-        if (user != null) {
-            welcomeMessage.setText("Bienvenido, " + user.getEmail());
-        }
-
-        buttonCategoria.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ListaCategoriasActivity.class));
-            }
-        });
-
-        buttonProveedor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ListaProveedoresActivity.class));
-            }
-        });
-
+        // BOTON PRODUCTO
         buttonProducto.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+
                 startActivity(new Intent(MainActivity.this, AgregarProductosActivity.class));
+
             }
         });
 
+        // BOTON PROVEEDOR
+        buttonProveedor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        buttonLogout.setOnClickListener(view -> logoutUser());
+                startActivity(new Intent(MainActivity.this, ListaProveedoresActivity.class));
+
+            }
+        });
+
+        // BOTON CATEGORIA
+        buttonCategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, ListaCategoriasActivity.class));
+
+            }
+        });
+
+        // BOTON REPORTE
+        buttonReporte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //startActivity(new Intent(MainActivity.this, ReporteActivity.class));
+
+            }
+        });
+
+        // BOTON REPORTE
+        buttonInventario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //startActivity(new Intent(MainActivity.this, InventarioActivity.class));
+
+            }
+        });
+
+        // Obtener usuario actual
+        //FirebaseUser user = auth.getCurrentUser();
+        //if (user != null) {
+        //    welcomeMessage.setText("Bienvenido, " + user.getEmail());
+        //}
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Agrega los ítems al menú
+        MenuItem itemCerrar = menu.add(Menu.NONE, 0, Menu.NONE, "Cerrar sesión");
+        itemCerrar.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);  // Menú desplegable
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0: // Cerrar sesión
+                logoutUser();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void logoutUser() {
@@ -65,3 +122,4 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 }
+
